fed9018cecce78ad6482e093db0eb667eb464870
LENOVO <mdzubeir2468@gmail.com>
2022-02-15 14:02:28 +0530

    Addition for declared values

---
+++ Assignment2/src/assignment2/additiondeclared.java
@@ -0,0 +1,22 @@
+package assignment2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class additiondeclared {
+
+	public static void main(String[] args) throws IOException{
+		int x = 35, y = 25,sum;
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		 
+		  x = Integer.parseInt(br.readLine());
+		 
+		  y = Integer.parseInt(br.readLine());
+		 
+		 sum = x+y;
+		 System.out.println("Sum of two numbers is \n" + sum);
+		 
+	}		 
+}
